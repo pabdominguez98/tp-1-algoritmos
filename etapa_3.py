58f5e478c766c12de550f0aac18a8f325084334a
Pablo Dominguez <thoriumsol@gmail.com>
2021-05-22 18:40:16 -0300

    Fase 3 lista

--- etapa_3.py
+++ etapa_3.py
@@ -1,10 +1,10 @@
 import random
 
 def seleccion_palabra(diccionario, longitud):
-    lista_palabras = diccionario.keys()
+    lista_palabras = list(diccionario.keys())
     lista_palabras_longitud = []
     for i in range (len(lista_palabras)):
-        if len(lista_palabras[i]) == len(longitud):
+        if len(lista_palabras[i]) == longitud:
             lista_palabras_longitud.append(lista_palabras[i]) 
     
     random_num = random.randint(0, len(lista_palabras_longitud) - 1)
@@ -14,8 +14,17 @@ def seleccion_palabra(diccionario, longitud):
 
 
 def main():
-    numero = random.randint(0,99)
+    diccionario = {
+        'carlos': 1,
+        'juan': 321,
+        'hola': '3',
+        'dsadsa': 3,
+        '4444':3
+    }
+    
+    print(seleccion_palabra(diccionario, 4))
 
-    print(numero)
+
+main()
 
 
